d133a825de52ead5f5272603c72c72cff5f5f397
Heinz <ateng.heinz@gmail.com>
2026-03-23 10:00:56 +0300

    2

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,8 +3,10 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\Role;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -17,9 +19,29 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        Role::create([
+            'name'=>'Admin',
         ]);
+         Role::create([
+                'name'=> 'User',
+        ]);
+
+        User::create([
+            'name' => 'Admin',
+            'email' => '233ateng@gmail.com',
+            'phone'=> '0781544283',
+            'delivery_address'=> 'N/A',
+            'role_id'=> 1,
+            'password'=> Hash::make('Qwerty1234'),
+                ]);
+
+        User::create([
+            'name' => 'User',
+            'email' => 'ateng.heinz@gmail.com',
+            'phone'=> '0701585836',
+            'delivery_address'=> 'N/A',
+            'role_id'=> 2,
+            'password'=> Hash::make('Qwerty1234'),
+                ]);
     }
 }
